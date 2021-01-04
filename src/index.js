1e217ff278a65acc58372968ebd87e85c6694162
mlkogon231 <mlkogon231@gmail.com>
2021-01-03 20:27:59 -0800

    first

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
-
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer
+});
 const logger = store => {
     return next => {
         return action => {
+
             console.log('[Middleware] Dispatching', action);
             const result = next(action);
             console.log('[Middleware] next state', store.getState());
@@ -23,7 +29,7 @@ const logger = store => {
     }
 }
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store={store}>
---
+++ src/store/actions/index.js
@@ -0,0 +1,2 @@
+export { addInredient, removeIngredient, initIngredients } from './burgerBuilder';
+export { purchaseBurger, purchaseInit } from './order';
